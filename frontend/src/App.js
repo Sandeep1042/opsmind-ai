d7d9127a5b5281fd9e28b54e0d059fce56b81f16
Sandeep1042 <sandeepjalandra183@gmail.com>
2025-12-22 19:01:42 +0530

    progress: PDF Upload + Parsing + Chunking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,10 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import UploadUI from "./components/UploadUI";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/")
-      .then(res => setMessage(res.data))
-      .catch(err => console.error(err));
-  }, []);
-
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-      <h1 className="text-2xl font-bold">Backend Response:</h1>
-      <p className="mt-4 text-blue-600">{message}</p>
+    <div className="min-h-screen bg-gray-100">
+      <UploadUI />
     </div>
   );
 }
